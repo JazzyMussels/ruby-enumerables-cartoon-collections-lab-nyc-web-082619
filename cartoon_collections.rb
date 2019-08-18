f59c2a39eda41db019278f9b84a126018ff4a49e
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-18 17:59:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,7 @@ def roll_call_dwarves(dwarf_names)
   dwarf_names.each{|x| puts x}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet
 end
 
 def long_planeteer_calls# code an argument here

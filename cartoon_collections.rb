934fe807de547fdd321f866f56252d3a3370917a
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-18 18:06:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  strings.find?{|ele|cheese_types.include?(ele)}
+  strings.find{|ele|cheese_types.include?(ele)}
 end
